145b8ed7ea7fdc1b3a6802b6fb5c1afca5c728a1
vemv <vemv@users.noreply.github.com>
2018-04-28 23:39:59 +0200

    Minor cleanup

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,7 @@ class User < ApplicationRecord
 
   has_many :recordings, dependent: :destroy
 
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
 
   validates :email, email_format: { message: "doesn't look like an email address" }, presence: true
   validates :name, presence: true
@@ -18,7 +15,6 @@ class User < ApplicationRecord
     Recording.where(user: self).each do |recording|
       words_arrays << recording.learning_words
     end
-    # create one array from all the arrays
     words = words_arrays.flatten
     # select only the words that have been at least 3 times
     known_words = words.select{|word| words.count(word) > 2 }
@@ -31,7 +27,7 @@ class User < ApplicationRecord
     Recording.where(user: self).each do |recording|
       words << recording.learning_words
     end
-    # the words that you are learning = total number new words but and no dublicated - the words that you already know.
+    # the words that you are learning = total number new words but and no duplicated - the words that you already know.
     words.uniq.flatten - words_known_by_user
   end
 
