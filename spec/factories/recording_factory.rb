d4d1bd9e476201b2a4c1c1f6a553a069a1500346
vemv <vemv@users.noreply.github.com>
2018-04-28 23:58:51 +0200

    Complete factories

--- spec/factories/recording_factory.rb
+++ spec/factories/recording_factory.rb
@@ -3,6 +3,7 @@ FactoryGirl.define do
   factory :recording do
 
     user
+    speaker 1
     data { "some json parsed to a string" }
     description { Faker::ChuckNorris.fact }
 
--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -3,6 +3,7 @@ FactoryGirl.define do
   factory :user do
 
     email { "#{SecureRandom.hex(8)}@mailinator.com" } # mail can be actually checked at mailinator.com.
+    name { SecureRandom.hex }
     password { SecureRandom.hex }
 
   end
